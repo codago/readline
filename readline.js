cee18c4cc030a611e6238ce5b8562022afa91272
zul <nasuhahijrah@gmail.com>
2017-08-16 12:50:10 +0700

    done

--- readline.js
+++ readline.js
@@ -7,35 +7,39 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Masukkan kalimat mu di sini? ', (kalimat) => {
-  // TODO: Log the answer in a database
+function sentencesManipulation(kalimat)
+{
+  var tampung = kalimat.split(' ');
+  var hasil   = [];
 
-  function sentencesManipulation(kalimat)
+  for (let i=0; i<tampung.length; i++)
   {
-    var tampung = kalimat.split(' ');
-    var hasil   = [];
-
-    for (let i=0; i<tampung.length; i++)
+    var awalKata = tampung[i].charAt(0);
+    if (awalKata=='a' || awalKata=='i' || awalKata=='u' || awalKata=='e' || awalKata=='o' )
     {
-      if (tampung[i].charAt(0)=='a' || tampung[i].charAt(0)=='i' || tampung[i].charAt(0)=='u' || tampung[i].charAt(0)=='e' || tampung[i].charAt(0)=='o' )
-      {
-        hasil.push(tampung[i]);
-      }
-      else
-      {
-        var awal = tampung[i].charAt(0);
-        var tambah = 'nyo';
-        var hapus = tampung[i].substr(1);
-        hasil.push(hapus.concat(awal,tambah));
-      }
+      hasil.push(tampung[i]);
     }
-      return hasil.join(" ");
+    else
+    {
+      var awal = tampung[i].charAt(0);
+      var tambah = 'nyo';
+      var hapus = tampung[i].substr(1);
+      hasil.push(hapus.concat(awal,tambah));
+    }
   }
+    return hasil.join(" ");
+}
 
-  console.log(`Hasil konversi:
-    ${
+console.log('Masukkan kalimat mu di sini?');
+rl.on('line', (kalimat) =>
+      {
         sentencesManipulation(kalimat)
-    }`);
+        console.log('\nHasil konversi:\n',sentencesManipulation(kalimat),'\n');
 
-  rl.close();
-});
+        rl.question(console.log('Masukkan kalimat mu di sini?'));
+      }
+    ).on('close',() =>
+          {
+           console.log('Good Bye!');
+           process.exit(0);
+          });
