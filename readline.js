040b338f51a4538cefbcc89755294cd8ba5081ae
arry kurnia <arrykurnia@arrys-MacBook-Pro.local>
2017-08-17 14:24:56 +0700

    fin

---
+++ readline.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.setPrompt('tulis kalimatmu disini >')
+ rl.prompt();
+
+ rl.on('line',(line) => {
+   console.log('hasil konversi : '+sentencesManipulation(line.trim()))
+
+  rl.prompt();
+
+}).on('close',() => {
+    console.log('Good Bye!');
+    process.exit(0);
+  });
+
+
+function stringManipulation(word) {
+  var vokal = ["a","e","i","o","u"];
+  var konsonan = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+  var result =''
+
+  vokal.forEach(function(element) {
+    if (element == word.toLowerCase().charAt(0)) {
+          result = word;
+    }
+  })
+  konsonan.forEach(function(element){
+    if (element == word.toLowerCase().charAt(0)) {
+        result = word.slice(1) + word.toLowerCase().charAt(0) + "nyo";
+    }
+  })
+  return result
+}
+
+function sentencesManipulation(sentence) {
+  var kalimat = sentence.split (" ");
+  var hasil = [];
+
+    for (var i = 0; i < kalimat.length; i++) {
+      hasil.push(stringManipulation(kalimat[i]));
+  }
+    return hasil.join(' ')
+}
