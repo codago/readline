95eb2bfda92d14e3db70a7568c9b78abb273ecb9
Abdul Azis <azis.h233@gmail.com>
2017-08-28 16:24:31 +0700

    selesai

---
+++ solution.js
@@ -0,0 +1,50 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.setPrompt('tulis kalimatmu di sini > ')
+rl.prompt();
+
+rl.on('line', (word) => {
+  console.log(`hasil konversi : ${sentencesManipulation(word.trim())}`);
+  rl.prompt();
+}).on('close', () => {
+  console.log('Good Bye');
+  process.exit(0);
+});
+
+function stringManipulation(word) {
+  //write your code here
+  var vokal = ["a", "i", "u", "e", "o" ];
+  var konsonan = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+                  "n", "p", "q", "r", "s", "t", "v", "w", "x" ,"y", "z"];
+
+  var string =''
+
+  vokal.forEach(function(replace){
+	if (replace === word.toLowerCase().charAt(0)){
+                string = word;
+	}
+   });
+
+  konsonan.forEach(function(replace){
+	 if (replace === word.toLowerCase().charAt(0)) {
+              string = word.slice(1) + word.toLowerCase().charAt(0) + "nyo";}})
+
+  return string;
+  }
+
+function sentencesManipulation(sentence){
+  var array = sentence.split(' ');
+  var tampung='';
+  for(var i=0; i<array.length;i++){
+    tampung+=stringManipulation(array[i])+' '
+
+  }
+
+
+  return tampung;
+}
