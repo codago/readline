d0456a012f487310504d0a8613aab99a7f172920
Bimo Bramantyo <bimo.bramantyo@gmail.com>
2017-09-03 14:00:58 +0700

    benerin error

--- readline.js
+++ readline.js
@@ -9,23 +9,21 @@ rl.setPrompt('tulis kalimatmu disini > ');
   rl.prompt();
 
 rl.on('line', function(line){
-  console.log('hasil conversi : ' + stringManipulation(line));
+  if(line.toLowerCase().match("good bye") !== null && line.toLowerCase().match("good bye").index === 1) {
+    process.exit()
+  } else {
+    console.log('hasil conversi : ' + stringManipulation(line));
     rl.prompt();
+  }
 })
 
 function stringManipulation(word) {
-	var vokal = ["a", "i", "u", "e", "o"];
-	var konsonan = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
-	var result = '';
-	vokal.forEach(function(element) {
-		if(element === word.toLowerCase().charAt(0)) {
-			result = word;
-    }
-  })
-	konsonan.forEach(function(element) {
-		if(element === word.toLowerCase().charAt(0)) {
-			result = word.slice(1) + word.toLowerCase().charAt(0) + "nyo";
+  var result = word.split(' ')
+
+  for(var x = 0; x<result.length; x++) {
+      if(result[x][0].toLowerCase().match(/a|i|u|e|o/) === null) {
+        result[x] = result[x].slice(1) + result[x].charAt(0) + "nyo";
       }
-    })
-	return result;
+  }
+  return result.join(' ');
 }
